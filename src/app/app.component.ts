66425b502f5dd30e9baba7334ee8b9f804d96866
Saifuddin Khan Afridi <git@github.com:NajumSoft/mobizonegit.git>
2021-07-24 13:43:39 +0500

    v1.00.01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontEnd';
+export class AppComponent implements OnInit {
+  constructor(private _http:HttpClient) {}
+
+  ngOnInit()
+  {
+    console.log("hello");
+  }
 }
