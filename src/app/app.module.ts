13b5a39d7ac4763b62d104e394e9ba81be3d8be4
unknown <saif.afridi62@gmail.com>
2021-08-01 16:00:52 -0700

    1.00.03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { MatButtonModule } from '@angular/material/button'
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -31,6 +32,7 @@ import { InvoiceAComponent } from './invoice-a/invoice-a.component';
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    ReactiveFormsModule,
     HttpClientModule,
     MatButtonModule,
     MatTooltipModule,
--- src/app/invoice-a/invoice-a.component.ts
+++ src/app/invoice-a/invoice-a.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-a',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceAComponent implements OnInit {
 
-  constructor() { }
+  InvoiceA_Form:FormGroup = new FormGroup({});
+  constructor(private fb:FormBuilder) { }
+
+
+
+
 
   ngOnInit(): void {
+     this.InvoiceA_Form = this.fb.group({
+       data:['',Validators.required],
+       customerName:['',Validators.required],
+       fatherName:['',Validators.required],
+       mobileNumber:['',Validators.required],
+       lisenceNumber:['',Validators.required],
+       address:['',Validators.required],
+       note:['',Validators.required],
+       invoiceDetail: this.fb.array([this.addSerial()]),
+     })
   }
 
+  get detailArray() {
+    return this.InvoiceA_Form.get('invoiceDetail') as FormArray;
+  }
+  addSerial():FormGroup{
+    return this.fb.group(
+{
+  serial:['',Validators.required],
+  remarks:['',Validators.required],
+  price:['',Validators.required],
+ }
+ 
+    )
+  }
+  status()
+  {
+   console.log(this.InvoiceA_Form.value);
+  }
 }
