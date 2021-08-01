a29745fa06cabfeec38240e802ceaffc1472beb4
unknown <saif.afridi62@gmail.com>
2021-08-01 05:41:17 -0700

    clear-all-bugs

--- src/app/invoice-a/invoice-a.component.ts
+++ src/app/invoice-a/invoice-a.component.ts
@@ -9,40 +9,46 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class InvoiceAComponent implements OnInit {
 
   InvoiceA_Form:FormGroup = new FormGroup({});
+  invoiceDetail:FormArray =  new FormArray([]);
   constructor(private fb:FormBuilder) { }
-
-
-
-
 
   ngOnInit(): void {
      this.InvoiceA_Form = this.fb.group({
-       data:['',Validators.required],
+       date:['',Validators.required],
        customerName:['',Validators.required],
        fatherName:['',Validators.required],
        mobileNumber:['',Validators.required],
        lisenceNumber:['',Validators.required],
        address:['',Validators.required],
        note:['',Validators.required],
-       invoiceDetail: this.fb.array([this.addSerial()]),
+       invoiceDetail: this.fb.array([this.makeSerial()]),
      })
   }
 
   get detailArray() {
     return this.InvoiceA_Form.get('invoiceDetail') as FormArray;
+
   }
-  addSerial():FormGroup{
+  makeSerial():FormGroup{
     return this.fb.group(
 {
   serial:['',Validators.required],
   remarks:['',Validators.required],
   price:['',Validators.required],
  }
+    
  
     )
+   
   }
-  status()
+  addSerial()
   {
-   console.log(this.InvoiceA_Form.value);
+   this.detailArray.push(this.makeSerial());
+  }
+  deleteSerial(i:any){
+    this.detailArray.removeAt(i);
+  }
+  submit(){
+    console.log(this.InvoiceA_Form.value);
   }
 }
