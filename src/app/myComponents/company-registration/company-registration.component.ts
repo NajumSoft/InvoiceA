58635160600e21a477d6064dd89e65ff5acbab21
saifuddin <saif.afridi62@gmail.com>
2022-02-28 14:55:14 +0500

    hello

--- src/app/myComponents/company-registration/company-registration.component.ts
+++ src/app/myComponents/company-registration/company-registration.component.ts
@@ -44,29 +44,36 @@ export class CompanyRegistrationComponent implements OnInit {
 
     }
     registerCompany(){
-  //    var formData = new FormData();
-  //    formData.append('file',this.myfile);
-  //    console.log(formData);
+     var emp ={
+       "Id":1,
+       "name":"raz",
+       "fname":"najum"
+     }
+
+      var formData = new FormData();
+      formData.append('emp',JSON.stringify(emp));
+      formData.append('file',this.myfile);
+    
+      console.log(formData);
    
-  //  //this._http.post('https://localhost:7268/FileUpload/UploadFile',formData).subscribe(d=>console.log(d));
-  //   this._http.get('https://localhost:7268/FileUpload/downloadFile?fileName=Stock.jpg',{ responseType:'blob'}).subscribe(d=>{
-  //           const reader = new FileReader();
+  this._http.post('https://localhost:7268/fileupload/uploadtofileshub',formData,).subscribe(d=>console.log(d));
+  //  this._http.get('https://localhost:7268/FileUpload/downloadFile?fileName=Stock.jpg',{ responseType:'blob'}).subscribe(d=>{
+  //          const reader = new FileReader();
   //           reader.readAsDataURL(d);
   //           reader.onload = (e) => this.imageSrc = e.target.result as string;
-  //           console.log(this.imageSrc);
+  //            console.log(this.imageSrc);
     
 
 
     
   //   });
 
-       var headers = new HttpHeaders({ 'Authorization': 'Bearer ' +this.token });
+      // var headers = new HttpHeaders({ 'Authorization': 'Bearer ' +this.token });
       //let options = new RequestOptions({ headers: headers });
-      this._http.get('http://api.najumsons.com/track/validatesecuredproduct/nac101',{ headers: headers }).subscribe(d=>console.log(d))
+    //  this._http.get('http://api.najumsons.com/track/validatesecuredproduct/nac101',{ headers: headers }).subscribe(d=>console.log(d))
 
-    };
+    }
    // console.log(   this._http.get('http://api.najumsons.com/track/validatesecuredproduct/nac101').subscribe(d=>console.log(d)));
   }
   
 
-  
